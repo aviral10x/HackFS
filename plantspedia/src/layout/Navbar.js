f317c01d5ebe42dc6846de02d66d878408f0d40f
Aviral <21f1000608@student.onlinedegree.iitm.ac.in>
2022-07-24 11:16:17 +0530

    plantspedia

--- plantspedia/src/layout/Navbar.js
+++ plantspedia/src/layout/Navbar.js
@@ -24,15 +24,15 @@ export default function NavBar({ }) {
                           >
                               {`${window.did?.slice(0, 16)}...`}{" "}
                               <svg
-                                  class="h-4 w-4 text-gray-500"
+                                  className="h-4 w-4 text-gray-500"
                                   width="24"
                                   height="24"
                                   viewBox="0 0 24 24"
-                                  stroke-width="2"
+                                  strokeWidth="2"
                                   stroke="currentColor"
                                   fill="none"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
                               >
                                   {" "}
                                   <path stroke="none" d="M0 0h24v24H0z" />{" "}
